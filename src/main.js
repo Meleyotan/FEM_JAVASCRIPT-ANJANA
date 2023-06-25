8a1ece60266fa737197b4835a57294ffd232097e
Meleyotan Oyeleke <meleyotanoyeleke@gmail.com>
2023-06-25 08:54:59 +0100

    Imported a file that contains quiz on concepts of JS

--- src/main.js
+++ src/main.js
@@ -1,3 +1,43 @@
 let x = "Meleyotan"
 console.log(x)
-console.log(_.endsWith(x,"n"))
+// console.log(_.endsWith(x,"n"))
+
+const menu = {
+    lunch:{
+        appetizer:"salad",
+        main: "spaghetti",
+        dessert: "tiramisu"
+    },
+    dinner:{
+        appetizer:"samosa",
+        main:"saag paneer",
+        dessert: "gulab juam"
+    }
+};
+
+const tiramisu = menu.lunch.dessert
+console.log(tiramisu) 
+
+
+const spices = [
+    {name:"Emma", nickname: "Baby"},
+    {name:"Geri", nickname: "Ginger"},
+    {name:"Mel B", nickname: "Scary"},
+    {name:"Mel C", nickname: "Sporty"},
+    {name: "Victoria", nickname: "Posh"}
+]
+const spiceGirls = {
+    albums:["Spice", "SpiceWorld", "Forever"],
+    motto:"Girl Power",
+    members:spices
+};
+
+console.log(spiceGirls.motto)
+console.log(spiceGirls.members[1].nickname)
+console.log(spiceGirls.albums[1])
+console.log(spiceGirls.members[4].name)
+
+const players = [
+    {name: "Player Y", symbol: "Y"},
+    {name: "Player Z", symbol: "Z"}
+]
