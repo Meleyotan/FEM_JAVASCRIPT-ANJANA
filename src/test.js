b0b794e9a0acffd5d7641860a5c8ef3a5f3ff215
Meleyotan Oyeleke <meleyotanoyeleke@gmail.com>
2023-06-30 00:36:57 +0100

    learnt how to reshuffle an array

---
+++ src/test.js
@@ -0,0 +1,15 @@
+const new_array = [];
+let i = 0;
+function test(something) {
+    while (new_array.length < 7) {
+        console.log("working")
+        console.log(new_array)
+        if (!new_array.includes(something[i])) {
+            new_array.push(something[i])
+        }
+        i = i + 1;
+  }
+  return new_array;
+}
+
+test([1,2,4,5,6,7])
