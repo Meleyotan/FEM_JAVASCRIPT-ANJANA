f2e564cdea14f86df9973574b9725e453acf614b
Meleyotan Oyeleke <meleyotanoyeleke@gmail.com>
2023-06-30 15:06:35 +0100

    using the fetch method

--- src/test.js
+++ src/test.js
@@ -19,3 +19,16 @@ test([1, 2, 4, 5, 6, 7])
 console.log("This will print first")
 setTimeout(() => { console.log("This will print last"), 1000 })
 console.log("This will print second")
+
+//Fetching data from APIs
+// let response = await fetch("https://dog.ceo/api/breed/hound/list")
+// let body = await response.json() 
+
+// console.log(body)
+// writing the above code like that will make it return a pending promise, we don't need a promise, we need
+// a response, then we use await to tell the browser to wait till it brings a response before it continues
+
+
+
+// or you can use the then method on the fetch
+let response1 = fetch("https://dog.ceo/api/breed/hound/list").then((value) => {console.log(value)})
